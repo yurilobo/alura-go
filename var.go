91ad3f64fda1f87853fe3c562397667e052d6b8a
Yuri <yuri.anderson.lobo@gamail.com>
2023-12-25 11:22:35 -0300

    Variaveis

---
+++ var.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Yuri"
+	var versao float32 = 1.1
+	var idade1 int
+	var idade2 int = 28
+	fmt.Println("Olá Sr.", nome, "nome, sua idade é:", idade1, "ou", idade2)
+	fmt.Println("Estamos na versão: ", versao)
+}
