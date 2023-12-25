ec2f4f7fcb4f807742adb76cdf7edff22f69e4ee
Yuri <yuri.anderson.lobo@gamail.com>
2023-12-25 11:28:44 -0300

    inferencia de tipos

--- var.go
+++ var.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var nome string = "Yuri"
-	var versao float32 = 1.1
+	var nome = "Yuri"
+	var versao = 1.1
 	var idade1 int
-	var idade2 int = 28
+	var idade2 = 28
 	fmt.Println("Olá Sr.", nome, "nome, sua idade é:", idade1, "ou", idade2)
 	fmt.Println("Estamos na versão: ", versao)
+	fmt.Println("Estamos usando o tipo : ", reflect.TypeOf(versao), " em versao")
 }
