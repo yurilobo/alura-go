ba8fe3d9acdf5f8cc1735042253f54f45109fc45
Yuri <yuri.anderson.lobo@gamail.com>
2023-12-25 15:06:13 -0300

    Atuando com funçoes

--- menu.go
+++ menu.go
@@ -6,19 +6,12 @@ import (
 )
 
 func main() {
-	nome := "Yuri"
-	versao := 1.1
 
-	fmt.Println("Olá Sr.", nome)
-	fmt.Println("Estamos na versão: ", versao)
-	fmt.Println("Estamos usando o tipo : ", reflect.TypeOf(versao), " em versao")
-	fmt.Println("1-Iniciar Monitoramento ")
-	fmt.Println("2-Exibir os logs ")
-	fmt.Println("0-Sair do programa ")
+	exibeIntroducao()
 
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi ", comando)
+	exibeMenu()
+
+	comando := lerComando()
 
 	// if comando == 1 {
 	// 	fmt.Println("Monitorando..")
@@ -40,4 +33,25 @@ func main() {
 		fmt.Println("Não conheco este comando")
 	}
 
+}
+func exibeIntroducao() {
+	nome := "Yuri"
+	versao := 1.1
+
+	fmt.Println("Olá Sr.", nome)
+	fmt.Println("Estamos na versão: ", versao)
+	fmt.Println("Estamos usando o tipo : ", reflect.TypeOf(versao), " em versao")
+
+}
+func exibeMenu() {
+	fmt.Println("1-Iniciar Monitoramento ")
+	fmt.Println("2-Exibir os logs ")
+	fmt.Println("0-Sair do programa ")
+}
+
+func lerComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi ", comandoLido)
+	return comandoLido
 }
