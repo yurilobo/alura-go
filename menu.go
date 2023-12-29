863bb60e99b04047ab3cba308a127e9669e54dfc
Yuri <yuri.anderson.lobo@gamail.com>
2023-12-29 15:25:02 -0300

    Logs

--- menu.go
+++ menu.go
@@ -134,7 +134,7 @@ func registraLog(site string, status bool) {
 		fmt.Println(err)
 	}
 
-	arquivo.WriteString(site + "=online" + strconv.FormatBool(status) + "\n")
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + "=online" + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
 }
